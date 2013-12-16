74c3e90db7274eca13827ff6ea7a1cc7e5109aff
robiso <r@robiso.com>
2013-12-16 23:05:46 +0100

    Create theme.php
    Each theme will now have it's own theme.php

---
+++ themes/blue/theme.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+<?php
+        echo "        <script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+        <title>$title - $page</title>
+        <meta charset='utf-8'>
+        <meta name='description' content='$description'>	
+        <meta name='keywords' content='$keywords'>
+        <meta name='generator' content='WonderCMS'>
+        <link rel='stylesheet' href='themes/$themeSelect/style.css'>";
+        editTags();
+?>
+</head>
+<body>
+        <header>
+                <nav id="nav">
+                        <h1 id="main-title"><a href='./'><?php echo $title;?></a></h1>
+                        <ul>
+                                <?php menu("<li><a","</a></li>");?>
+                        </ul>
+                </nav>
+        </header>
+
+        <?php if(is_loggedin()) settings();?>
+
+        <div class="clear"></div>
+        <div id="wrapper">
+                        <?php mainContent();?>
+        </div>
+        
+        <div id="side">
+                        <?php subContent();?>
+        </div>
+
+        <div class="clear"></div>
+        <footer><p><?php echo "$copyright | $mess | $lstatus";?></p></footer>
+</body>
+</html>
