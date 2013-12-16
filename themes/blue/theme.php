bc59dcc84036268a4e9a09afc36fd56cf0f09388
robiso <r@robiso.com>
2013-12-16 23:06:29 +0100

    Update theme.php
    Updated theme.php

--- themes/blue/theme.php
+++ themes/blue/theme.php
@@ -2,38 +2,37 @@
 <html lang="en">
 <head>
 <?php
-        echo "        <script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
-        <title>$title - $page</title>
-        <meta charset='utf-8'>
-        <meta name='description' content='$description'>	
-        <meta name='keywords' content='$keywords'>
-        <meta name='generator' content='WonderCMS'>
-        <link rel='stylesheet' href='themes/$themeSelect/style.css'>";
-        editTags();
+	echo "	<script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+	<title>".$c['title']." - ".$c['page']."</title>
+	<meta charset='utf-8'>
+	<meta name='description' content='".$c['description']."'>
+	<meta name='keywords' content='".$c['keywords']."'>
+	<link rel='stylesheet' href='themes/".$c['themeSelect']."/style.css'>";
+	editTags();
 ?>
 </head>
 <body>
-        <header>
-                <nav id="nav">
-                        <h1 id="main-title"><a href='./'><?php echo $title;?></a></h1>
-                        <ul>
-                                <?php menu("<li><a","</a></li>");?>
-                        </ul>
-                </nav>
-        </header>
+	<header>
+		<nav id="nav">
+			<h1 id="main-title"><a href='./'><?php echo $c['title'];?></a></h1>
+			<ul>
+				<?php menu("<li><a","</a></li>");?>
+			</ul>
+		</nav>
+	</header>
 
-        <?php if(is_loggedin()) settings();?>
+	<?php if(is_loggedin()) settings();?>
 
-        <div class="clear"></div>
-        <div id="wrapper">
-                        <?php mainContent();?>
-        </div>
-        
-        <div id="side">
-                        <?php subContent();?>
-        </div>
+	<div class="clear"></div>
+	<div id="wrapper">
+			<?php content($c['page'],$c['content']);?>
+	</div>
+	
+	<div id="side">
+			<?php content('subside',$c['subside']);?>
+	</div>
 
-        <div class="clear"></div>
-        <footer><p><?php echo "$copyright | $mess | $lstatus";?></p></footer>
+	<div class="clear"></div>
+	<footer><p><?php echo $c['copyright'] ." | $sig | $lstatus";?></p></footer>
 </body>
 </html>
