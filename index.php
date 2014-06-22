d617e817403f5407c7fbecf964aaf6d429bf64a2
robiso <r@robiso.com>
2014-06-22 23:23:28 +0200

    index.php re-vamp
    Main change: editText.php is now merged into this index.php.
    
    IMPORTANT CHANGES:
    1. Menu re-vamp. You will now only need <?php menu(); ?> in your theme.php instead of the old code.
    2. hostname renamed to host. If you're updating from an older BETA, remember to rename hostname to host in your theme.php (in head -> used in <base..).
    
    Other notes: A few code optimizations (trimming text and simplifying where possible).
    Also added auto create two folders (files/plugins) if files folder is missing. We're checking only one because it something would have to be really wrong if only one of those two folders is missing + we save space.

--- index.php
+++ index.php
@@ -1,56 +1,82 @@
 <?php
+ob_start();
+ini_set('session.cookie_httponly', 1);
 session_start();
+host();
+edit();
 
-$hostname = str_replace($_REQUEST['page'], '', str_replace('index.php', '', $_SERVER['PHP_SELF']));
 $c['password'] = 'admin';
 $c['loggedin'] = false;
-$c['page'] = 'Home';
-$d['page']['Home'] = "<h3>Congratulations! Your website is now powered by WonderCMS.</h3><br />\nLogin to the admin panel with the 'Login' link in the footer. The default password is admin.<br />\nChange the password as soon as possible.<br /><br />\n\nOnce logged in, click on the content to edit it and click outside to save it.<br /><br />\n\nWonderCMS weights only around 15kB. (8kB zipped)";
-$d['page']['Example'] = "This is an example page.<br /><br />\n\nTo add a new one, click on the existing pages (in the admin panel) and enter a new one below the others.";
-$d['new_page']['admin'] = "Page <b>".str_replace('-',' ',$_REQUEST['page'])."</b> created.<br /><br />\n\nClick here to start editing!";
-$d['new_page']['visitor'] = "Sorry, but <b>".str_replace('-',' ',$_REQUEST['page'])."</b> doesn't exist. :(";
-$d['default']['content'] = "Click here to edit!";
-$c['themeSelect'] = "blue";
+$c['page'] = 'home';
+$d['page']['home'] = "<h3>It's alive! Your website is now powered by WonderCMS.</h3><br />\nLogin with the 'Login' link below. The password is admin.<br />\nChange the password as soon as possible.<br /><br />\n\nClick on the content to edit and click outside to save it.<br /><br />\n\nWonderCMS weights only around 15kB. (8kB zipped)";
+$d['page']['example'] = "This is an example page.<br /><br />\n\nTo add a new one, click on the existing pages (in the admin panel) and enter a new one below the others.";
+$d['new_page']['admin'] = "Page <sb>".$rp."</b> created.<br /><br />\n\nClick here to start editing!";
+$d['new_page']['visitor'] = "Sorry, but <b>".$rp."</b> doesn't exist. :(";
+$d['default']['content'] = 'Click to edit!';
+$c['themeSelect'] = 'reponsive-blue';
 $c['menu'] = "Home<br />\nExample";
 $c['title'] = 'Website title';
-$c['subside'] = "<h3>ABOUT YOUR WEBSITE</h3><br />\nThis box can contain your photo, website description, contact information, mini map or anything else.<br /><br />\n\n The content is static and visible on all pages.";
-$c['description'] = 'Enter website description.';
-$c['keywords'] = 'Enter, your, website, keywords.';
-$c['copyright'] = '&copy;'. date('Y') .' Your website';
+$c['subside'] = "<h3>ABOUT YOUR WEBSITE</h3><br />\nYour photo, website description, contact information, mini map or anything else.<br /><br />\n\n This content is static and is visible on all pages.";
+$c['description'] = 'Your website description.';
+$c['keywords'] = 'enter, your website, keywords';
+$c['copyright'] = '&copy;'.date('Y').' Your website';
 $sig = "Powered by <a href='http://wondercms.com'>WonderCMS</a>";
 $hook['admin-richText'] = "rte.php";
 
+if(!file_exists('files')){
+	mkdir('files', 0755, true);
+	mkdir('plugins', 0755, true);
+}
+
 foreach($c as $key => $val){
-	if($key == 'content')continue;
+	if($key == 'content') continue;
 	$fval = @file_get_contents('files/'.$key);
 	$d['default'][$key] = $c[$key];
-	if($fval)$c[$key] = $fval;
+	if($fval)
+		$c[$key] = $fval;
 	switch($key){
 		case 'password':
-			if(!$fval)$c[$key] = savePassword($val);
+			if(!$fval)
+				$c[$key] = savePassword($val);
 			break;
 		case 'loggedin':
-			if($_SESSION['l']==$c['password'])$c[$key] = true;
+			if(isset($_SESSION['l']) and $_SESSION['l'] == $c['password'])
+				$c[$key] = true;
 			if(isset($_REQUEST['logout'])){
 				session_destroy();
 				header('Location: ./');
 				exit;
 			}
 			if(isset($_REQUEST['login'])){
-				if(is_loggedin())header('Location: ./');
-				loginForm();
+				if(is_loggedin())
+					header('Location: ./');
+				$msg = '';
+				if(isset($_POST['sub']))
+					login();
+				$c['content'] = "<form action='' method='POST'>
+				<input type='password' name='password'>
+				<input type='submit' name='login' value='Login'> $msg
+				<p class='toggle'>Change password</p>
+				<div class='hide'>Type your old password above and your new one below.<br />
+				<input type='password' name='new'>
+				<input type='submit' name='login' value='Change'>
+				<input type='hidden' name='sub' value='sub'>
+				</div>
+				</form>";
 			}
-			$lstatus = (is_loggedin())? "<a href='$hostname?logout'>Logout</a>": "<a href='$hostname?login'>Login</a>";
+			$lstatus = (is_loggedin()) ? "<a href='$host?logout'>Logout</a>" : "<a href='$host?login'>Login</a>";
 			break;
 		case 'page':
-			if($_REQUEST['page'])$c[$key]=$_REQUEST['page'];
+			if($rp)
+				$c[$key] = $rp;
 			$c[$key] = getSlug($c[$key]);
-			if(isset($_REQUEST['login']))continue;
+			if(isset($_REQUEST['login'])) continue;
 			$c['content'] = @file_get_contents("files/".$c[$key]);
 			if(!$c['content']){
 				if(!isset($d['page'][$c[$key]])){
-						$c['content'] = (is_loggedin())? $d['new_page']['admin']:$c['content'] = $d['new_page']['visitor'];
-				}else{
+					header('HTTP/1.1 404 Not Found');
+					$c['content'] = (is_loggedin()) ? $d['new_page']['admin'] : $c['content'] = $d['new_page']['visitor'];
+				} else{
 					$c['content'] = $d['page'][$c[$key]];
 				}
 			}
@@ -64,20 +90,21 @@ loadPlugins();
 require("themes/".$c['themeSelect']."/theme.php");
 
 function loadPlugins(){
-	global $hook,$hostname,$c;
+	global $hook, $c;
 	$cwd = getcwd();
 	if(chdir("./plugins/")){
 		$dirs = glob('*', GLOB_ONLYDIR);
-		foreach($dirs as $dir){
-			require_once($cwd.'/plugins/'.$dir.'/index.php');
-		}
+		if(is_array($dirs))
+			foreach($dirs as $dir){
+				require_once($cwd.'/plugins/'.$dir.'/index.php');
+			}
 	}
 	chdir($cwd);
-	$hook['admin-head'][] = "<script type='text/javascript' src='./js/editInplace.php?hook=".$hook['admin-richText']."'></script>";
+	$hook['admin-head'][] = "\n	<script type='text/javascript' src='./js/editInplace.php?hook=".$hook['admin-richText']."'></script>";
 }
 
 function getSlug($p){
-	return $slug;
+	return mb_convert_case(str_replace(' ', '-', $p), MB_CASE_LOWER, "UTF-8");
 }
 
 function is_loggedin(){
@@ -87,65 +114,70 @@ function is_loggedin(){
 
 function editTags(){
 	global $hook;
-	if(!is_loggedin() && !isset($_REQUEST['login'])) return;
+	if(!is_loggedin() && !isset($_REQUEST['login']))
+		return;
 	foreach($hook['admin-head'] as $o){
 		echo "\t".$o."\n";
 	}
 }
 
-function content($id,$content){
+function content($id, $content){
 	global $d;
-	echo (is_loggedin())? "<span title='".$d['default']['content']."' id='".$id."' class='editText richText'>".$content."</span>": $content;
+	echo (is_loggedin()) ? "<span title='".$d['default']['content']."' id='".$id."' class='editText richText'>".$content."</span>" : $content;
 }
 
-function menu($stags,$etags){
-	global $c;
-	$mlist = explode('<br />',$c['menu']);
-	for($i=0;$i<count($mlist);$i++){
-		$page = getSlug($mlist[$i]);
-		if(!$page) continue;
-		echo $stags." href='".$page."'>".str_replace('-',' ',$page)." ".$etags." \n";
+function edit(){
+	if(isset($_REQUEST['fieldname'], $_REQUEST['content'])){
+		$fieldname = $_REQUEST['fieldname'];
+		$content = trim(rtrim(stripslashes($_REQUEST['content'])));
+		if(!isset($_SESSION['l'])){
+			header('HTTP/1.1 401 Unauthorized');
+			exit;
+		}
+		$file = @fopen("files/$fieldname", "w");
+		if(!$file){
+			echo 'Set 755 permission to the files folder.';
+			exit;
+		}
+		fwrite($file, $content);
+		fclose($file);
+		echo $content;
+		exit;
 	}
 }
-	
-function loginForm(){
-	global $c, $msg;
-	$msg = '';
-	if(isset($_POST['sub'])) login();
-	$c['content'] = "<form action='' method='POST'>
-	Password <input type='password' name='password'>
-	<input type='submit' name='login' value='Login'> $msg
-	<br /><br /><b class='toggle'>Change password</b>
-	<div class='hide'><br />Type your old password above and your new one below.<br />
-	New Password <input type='password' name='new'>
-	<input type='submit' name='login' value='Change'>
-	<input type='hidden' name='sub' value='sub'>
-	</div>
-	</form>";
+
+function menu(){
+	global $c, $host;
+	$mlist = explode("<br />\n", $c['menu']);
+	?><ul>
+	<?php
+	foreach ($mlist as $cp){¸?>
+			<li<?php if($c['page'] == getSlug($cp)) echo ' id="active" '; ?>><a href='<?php echo getSlug($cp); ?>'><?php echo $cp; ?></a></li>
+	<?php } ?>
+	</ul>
+<?php
 }
-	
+
 function login(){
 	global $c, $msg;
-	if(md5($_POST['password'])<>$c['password']){
-		$msg = "<b>Wrong Password</b>";
+	if(md5($_POST['password']) <> $c['password']){
+		$msg = 'wrong password';
 		return;
 	}
 	if($_POST['new']){
 		savePassword($_POST['new']);
-		$msg = 'Password changed';
+		$msg = 'password changed';
 		return;
 	}
 	$_SESSION['l'] = $c['password'];
 	header('Location: ./');
 	exit;
 }
-	
+
 function savePassword($p){
 	$file = @fopen('files/password', 'w');
 	if(!$file){
-		echo "<h3>Unable to access password</h3>".
-			"Set correct permissions (640) to the password file (in the 'files' directory).<br /><br />
-			<a href='http://wondercms.com/forum'>For support, click here.</a>";
+		echo 'Set 644 permission to the password file.';
 		exit;
 	}
 	fwrite($file, md5($p));
@@ -153,24 +185,38 @@ function savePassword($p){
 	return md5($p);
 }
 
+function host(){
+	global $host, $rp;
+	$rp = preg_replace('#/+#', '/', (isset($_REQUEST['page'])) ? urldecode($_REQUEST['page']) : '');
+	$host = $_SERVER['HTTP_HOST'];
+	$uri = preg_replace('#/+#', '/', urldecode($_SERVER['REQUEST_URI']));
+	$host = (strrpos($uri, $rp) !== false) ? $host.'/'.substr($uri, 0, strlen($uri) - strlen($rp)) : $host.'/'.$uri;
+	$host = explode('?', $host);
+	$host = '//'.str_replace('//', '/', $host[0]);
+	$strip = array('index.php','?','"','\'','>','<','=','(',')','\\');
+	$rp = strip_tags(str_replace($strip, '', $rp));
+	$host = strip_tags(str_replace($strip, '', $host));
+}
+
 function settings(){
-	global $c,$d;
+	global $c, $d;
 	echo "<div class='settings'>
 	<h3 class='toggle'>↕ Settings ↕</h3>
 	<div class='hide'>
-	<div class='change'><b>Theme</b>&nbsp;<span id='themeSelect'><select name='themeSelect' onchange='fieldSave(\"themeSelect\",this.value);'>";
+	<div class='change border'><b>Theme</b>&nbsp;<span id='themeSelect'><select name='themeSelect' onchange='fieldSave(\"themeSelect\",this.value);'>";
 	if(chdir("./themes/")){
 		$dirs = glob('*', GLOB_ONLYDIR);
 		foreach($dirs as $val){
-			$select = ($val == $c['themeSelect'])? ' selected' : ''; 
+			$select = ($val == $c['themeSelect']) ? ' selected' : '';
 			echo '<option value="'.$val.'"'.$select.'>'.$val."</option>\n";
 		}
 	}
 	echo "</select></span></div>
-	<div class='change'><b>Navigation <small>(hint: add your own page below and <a href='javascript:location.reload(true);'>click here to refresh</a>)</small></b><br /><span id='menu' title='Home' class='editText'>".$c['menu']."</span></div>";
+	<div class='change border'><b>Menu <small>(add a page below and <a href='javascript:location.reload(true);'>refresh</a>)</small></b><span id='menu' title='Home' class='editText'>".$c['menu']."</span></div>";
 	foreach(array('title','description','keywords','copyright') as $key){
-		echo "<div class='change'><span title='".$d['default'][$key]."' id='".$key."' class='editText'>".$c[$key]."</span></div>";
+		echo "<div class='change border'><span title='".$d['default'][$key]."' id='".$key."' class='editText'>".$c[$key]."</span></div>";
 	}
 	echo "</div></div>";
 }
+ob_end_flush();
 ?>
