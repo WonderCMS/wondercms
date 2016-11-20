cca6606ab2297996e2b6ab68df432df2e261e828
robiso <r@robiso.com>
2016-11-20 21:04:30 +0100

    Update index.php
    Patching uppercased PASSWORD discloas (hash disclosal), thanks to https://github.com/yassineaddi for finding it. Will be added to the thank you list!

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ foreach ($wCMS->config as $key => $val) {
 				}
 			}
 
-			if (strpos($_SERVER['REQUEST_URI'], 'password') !== false) {
+			if (strpos($_SERVER['REQUEST_URI'], 'password') !== false || strpos($_SERVER['REQUEST_URI'], 'PASSWORD') !== false)  {
 				header('Location: ./');
 				exit;
 			}
