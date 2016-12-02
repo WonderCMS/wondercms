ee9278807b5fe8c848899e4c34afe82ec5fc6f4b
robiso <r@robiso.com>
2016-12-02 03:08:08 +0100

    Redundant

--- js/richTextEditor.php
+++
@@ -1,2 +0,0 @@
-a.html("<textarea "+title+" id=\""+ a.attr('id') +"_field\" onblur=\"fieldSave(a.attr('id'),nl2br(this.value));\">" + a.html().replace(/<br>/gi, "") + "</textarea>");
-a.children(':first').focus();
