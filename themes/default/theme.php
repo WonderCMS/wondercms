638a836cfbfb7ad7ce455c4bbf5296d57719c263
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-02-10 22:02:08 +0100

    Move the class in its own file and other major changes (#101)
    * First commit of the major changes (WIP)
    
    * add composer.json with symfony/http-foundation dependency
    At the moment this is only used once but usage will probably augment in
    time
    * create src/ and move css and js ind there, also the Wcms class
    This separate better what is the source code of the project vs what
    should be served to the users
    * add namespace to the project
    * remove static keyword from methods
    * add type hints everywhere (almost)
    * bring psalm static analysis results from 48 errors and 35 issues to 0 errors and 4 issues
    * fix all errors reported by phpstan static analyzer up to level 6
    * make the whole backup related code cleaner
    * rename db() in getDb() as db is also a property
    Also make the db property useful, make the save() write to disk what is is db property
    * rewrite some code to make it simpler, cleaner, faster, better
    * use strict comparison everywhere
    
    * fix issues reported by scrutinizer
    
    * use getScheme()
    
    * make url() static
    
    * Add php version and extensions requirements in composer.json
    
    * Remove debug code
    
    * Add try/catch in index.php

--- themes/default/theme.php
+++ themes/default/theme.php
@@ -1,3 +1,4 @@
+<?php global $Wcms ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,19 +6,19 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title><?= wCMS::get('config','siteTitle') ?> - <?= wCMS::page('title') ?></title>
-        <meta name="description" content="<?= wCMS::page('description') ?>">
-        <meta name="keywords" content="<?= wCMS::page('keywords') ?>">
+        <title><?= $Wcms->get('config','siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+        <meta name="description" content="<?= $Wcms->page('description') ?>">
+        <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="<?= wCMS::asset('css/style.css') ?>">
-        <?= wCMS::css() ?>
+        <link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
+        <?= $Wcms->css() ?>
 
     </head>
 
     <body>
-        <?= wCMS::alerts() ?>
-        <?= wCMS::settings() ?>
+        <?= $Wcms->alerts() ?>
+        <?= $Wcms->settings() ?>
 
         <nav class="navbar navbar-default">
             <div class="container">
@@ -26,15 +27,15 @@
                         <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
                     </button>
 
-                    <a class="navbar-brand" href="<?= wCMS::url() ?>">
-                        <?= wCMS::get('config','siteTitle') ?>
+                    <a class="navbar-brand" href="<?= $Wcms->url() ?>">
+                        <?= $Wcms->get('config','siteTitle') ?>
 
                     </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="menu-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <?= wCMS::menu() ?>
+                        <?= $Wcms->menu() ?>
 
                     </ul>
                 </div>
@@ -44,7 +45,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center padding40">
-                    <?= wCMS::page('content') ?>
+                    <?= $Wcms->page('content') ?>
 
                 </div>
             </div>
@@ -53,7 +54,7 @@
         <div class="container-fluid blueBackground whiteFont">
             <div class="row">
                 <div class="col-lg-12 text-center padding40">
-                    <?= wCMS::block('subside') ?>
+                    <?= $Wcms->block('subside') ?>
 
                 </div>
             </div>
@@ -61,14 +62,14 @@
 
         <footer class="container-fluid">
             <div class="text-right padding20">
-                <?= wCMS::footer() ?>
+                <?= $Wcms->footer() ?>
 
             </div>
         </footer>
 
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <?= wCMS::js() ?>
+        <?= $Wcms->js() ?>
 
     </body>
 </html>
