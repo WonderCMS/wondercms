7644629088b52cb84410348ee8ac1c6d75daba41
Robert Isoski <r@robiso.com>
2019-03-31 18:56:11 +0200

    Update summernote.php
    - Adding correct folder structure/path.
    Summernote should now work with wcms v3.0.0

--- plugins/summernote/summernote.php
+++ plugins/summernote/summernote.php
@@ -8,9 +8,6 @@
  * @forked by Robert Isoski @robertisoski
  * @version 3.0.0
  */
-namespace Robiso\Wondercms;
-
-//require_once 'vendor/autoload.php';
 
 global $Wcms;
 
@@ -48,8 +45,7 @@ function loadSummerNoteJS($args) {
     if ($Wcms->loggedIn) {
         $script = <<<'EOT'
         <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.8/dist/summernote.min.js" integrity="sha384-rv18XCwOLyh2rO/6Vz9eKWaSP+ljKfFed/npYlSg476id+996jfNDF+5iC3W5KKJ" crossorigin="anonymous"></script>
-        <script src="plugins/summernote/js/admin.js"></script>
-        <script src="plugins/summernote/js/files.js"></script>
+        <script src="data/plugins/summernote/js/admin.js"></script>
 EOT;
     }
     $args[0] .= $script;
@@ -61,7 +57,7 @@ function loadSummerNoteCSS($args) {
     if ($Wcms->loggedIn) {
         $script = <<<'EOT'
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/summernote@0.8.11/dist/summernote.css" integrity="sha384-hHEPAd3Dkb316VuNPtIZ6LUrzxvD4PQOTW478Ww6c/aUJKXNDV9pEx5/jZgISR1G" crossorigin="anonymous">
-        <link rel="stylesheet" href="plugins/summernote/css/admin.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="data/plugins/summernote/css/admin.css" type="text/css" media="screen">
 EOT;
     }
     $args[0] .= $script;
