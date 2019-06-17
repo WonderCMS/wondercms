73895e7931cf4f2272c0ac1b937f87f146919d40
slaven <stancic.slaven@gmail.com>
2019-06-17 20:03:46 +0200

    Added new JS, small index php fixes

--- index.php
+++ index.php
@@ -432,9 +432,9 @@ EOT;
     /**
      * Get content of the database
      *
-     * @return object
+     * @return stdClass
      */
-    public function getDb(): object
+    public function getDb(): stdClass
     {
         // initialize the database if it doesn't exist yet
         if (!\file_exists($this->dbPath)) {
@@ -714,7 +714,7 @@ EOT;
 <script src="https://cdn.jsdelivr.net/npm/autosize@4.0.2/dist/autosize.min.js" integrity="sha384-gqYjRLBp7SeF6PCEz2XeqqNyvtxuzI3DuEepcrNHbrO+KG3woVNa/ISn/i8gGtW8" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/taboverride@4.0.3/build/output/taboverride.min.js" integrity="sha384-fYHyZra+saKYZN+7O59tPxgkgfujmYExoI6zUvvvrKVT1b7krdcdEpTLVJoF/ap1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery.taboverride@4.0.0/build/jquery.taboverride.min.js" integrity="sha384-RU4BFEU2qmLJ+oImSowhm+0Py9sT+HUD71kZz1i0aWjBfPx+15Y1jmC8gMk1+1W4" crossorigin="anonymous"></script>
-<script>function nl2br(a){return(a+"").replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,"$1<br>$2")}function fieldSave(a,b,c,d,e){$("#save").show(),$.post("",{fieldname:a,token:token,content:b,target:c,menu:d,visibility:e},function(a){}).always(function(){window.location.reload()})}$(document).tabOverride(!0,"textarea");var changing=!1;$(document).ready(function(){$("body").on("click","div.editText",function(){changing||(a=$(this),title=a.attr("title")?title='"'+a.attr("title")+'" ':"",a.hasClass("editable")?a.html("<textarea "+title+' id="'+a.attr("id")+"_field\" onblur=\"fieldSave(a.attr('id'),this.value,a.data('target'),a.data('menu'),a.data('visibility'));\">"+a.html()+"</textarea>"):a.html("<textarea "+title+' id="'+a.attr("id")+"_field\" onblur=\"fieldSave(a.attr('id'),nl2br(this.value),a.data('target'),a.data('menu'),a.data('visibility'));\">"+a.html().replace(/<br>/gi,"\n")+"</textarea>"),a.children(":first").focus(),autosize($("textarea")),changing=!0)}),$("body").on("click","i.menu-toggle",function(){var a=$(this),c=(setTimeout(function(){window.location.reload()},500),a.attr("data-menu"));a.hasClass("menu-item-hide")?(a.removeClass("glyphicon-eye-open menu-item-hide").addClass("glyphicon-eye-close menu-item-show"),a.attr("title","Hide page from menu").attr("data-visibility","hide"),$.post("",{fieldname:"menuItems",token:token,content:" ",target:"menuItemVsbl",menu:c,visibility:"hide"},function(a){})):a.hasClass("menu-item-show")&&(a.removeClass("glyphicon-eye-close menu-item-show").addClass("glyphicon-eye-open menu-item-hide"),a.attr("title","Show page in menu").attr("data-visibility","show"),$.post("",{fieldname:"menuItems",token:token,content:" ",target:"menuItemVsbl",menu:c,visibility:"show"},function(a){}))}),$("body").on("click",".menu-item-add",function(){var newPage=prompt("Enter page name");if(!newPage)return!1;newPage=newPage.replace(/[`~;:'",.<>\{\}\[\]\\\/]/gi,"").trim(),$.post("",{fieldname:"menuItems",token:token,content:newPage,target:"menuItem",menu:"none",visibility:"show"},function(a){}).done(setTimeout(function(){window.location.reload()},500))}),$("body").on("click",".menu-item-up,.menu-item-down",function(){var a=$(this),b=a.hasClass("menu-item-up")?"-1":"1",c=a.attr("data-menu");$.post("",{fieldname:"menuItems",token:token,content:b,target:"menuItemOrder",menu:c,visibility:""},function(a){}).done(function(){$("#menuSettings").parent().load("index.php #menuSettings",{func:"getMenuSettings"})})})});</script>
+<script src=“https://cdn.jsdelivr.net/gh/robiso/wondercms-files@3.0.0/wcms-admin.min.js” integrity=“sha384-VbizrCbxrsbdpVYH2abqlWxSCc3qhyFErT0X3mNyVuWay72hHRnKlUNPAHgxS0/o” crossorigin=“anonymous”></script>
 EOT;
             $scripts .= '<script>let token = "' . $this->getToken() . '";</script>';
             return $this->hook('js', $scripts)[0];
@@ -730,10 +730,10 @@ EOT;
     private function loadPlugins(): void
     {
         $plugins = $this->rootDir . '/plugins';
-        if (!mkdir($plugins) && !is_dir($plugins)) {
+        if (!is_dir($plugins) && !mkdir($plugins) && !is_dir($plugins)) {
             return;
         }
-        if (!mkdir($this->filesPath) && !is_dir($this->filesPath)) {
+        if (!is_dir($this->filesPath) && !mkdir($this->filesPath) && !is_dir($this->filesPath)) {
             return;
         }
         foreach (glob($plugins . '/*', GLOB_ONLYDIR) as $dir) {
