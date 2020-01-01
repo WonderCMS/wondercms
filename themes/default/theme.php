4ec01dd9c337228ab1a5250b58ab22f5572ba076
Robert Isoski <r@robiso.com>
2020-01-01 23:21:24 +0100

    Update theme.php

--- themes/default/theme.php
+++ themes/default/theme.php
@@ -7,7 +7,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<title><?= $Wcms->get('config', 'siteTitle') ?> -	<?= $Wcms->page('title') ?></title>
+		<title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
 		<meta name="description" content="<?= $Wcms->page('description') ?>">
 		<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
