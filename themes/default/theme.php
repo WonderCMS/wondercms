85cd9ba350f6b0c88fd05c6471bc60c1cfed667c
Slaven Stancic <stancic.slaven@gmail.com>
2020-02-10 19:47:43 +0100

    - Theme css location fix

--- themes/default/theme.php
+++ themes/default/theme.php
@@ -12,9 +12,8 @@
 		<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<?= $Wcms->css() ?>
+        <?= $Wcms->css() ?>
         <link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
-
 	</head>
 
     <body>
